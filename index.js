64086a5cbcb9674b3966bda4f352dfd9d8fff8fd
GrinJessie <lijing@ualberta.ca>
2018-04-02 00:53:23 +0000

    change from html to ejs, express serve public folder, change links path

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+
+express()
+  .use(express.static(path.join(__dirname, '/public/')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .get('/about', (req, res) => res.render('about'))
+  .get('/work', (req, res) => res.render('work'))
+  .get('/blog', (req, res) => res.render('blog'))
+  .get('/contact', (req, res) => res.render('contact'))
+
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
